ea2bf1bc6e7ded5da26f38e7cdec735b49718345
nickmenegussi <nicolasmramos09@gmail.com>
2024-12-24 14:31:14 -0300

    Atualizando parâmetros de rota e consultas no TopicController para utilizar 'topicId' e adicionar verificações de usuário nas operações de atualização e exclusão de tópicos.

---
+++ BackEnd/src/routers/TopicRouter.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const {viewAllTopic, viewOnlyTopicById, createTopic, updateTitle, updateDescription, updateTopicImage, deleteTopic} = require('../controllers/TopicController')
+const authMiddleware = require('../middleware/authMidleware')
+const upload = require('../multerConfig/multer')
+const verifyPermission = require('../middleware/roleMiddleware')
+
+router.get('/topic', authMiddleware, viewAllTopic)
+router.get('/topic/:topicId', authMiddleware, viewOnlyTopicById)
+
+router.post('/topic/create', authMiddleware, upload.single('imagem'), createTopic)
+
+router.patch('/topic/:topicId/title', authMiddleware, updateTitle)
+router.patch('/topic/:topicId/description', authMiddleware, updateDescription)
+router.patch('/topic/:topicId/image', authMiddleware, upload.single('imagem'), updateTopicImage)
+
+router.delete('/topic/:topicId/delete', authMiddleware, verifyPermission(['Admin', 'SuperAdmin']), deleteTopic)
