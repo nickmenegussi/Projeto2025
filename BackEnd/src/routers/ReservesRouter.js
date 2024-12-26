273f38b95e66453eaeac27c34a2aca833f910fc8
nickmenegussi <nicolasmramos09@gmail.com>
2024-12-25 23:40:09 -0300

    Atualizando o ReservesController e ReservesRouter para incluir verificações de permissão baseadas em roles de usuário e ajustando rotas para aceitar parâmetros de usuário e carrinho.

--- BackEnd/src/routers/ReservesRouter.js
+++ BackEnd/src/routers/ReservesRouter.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const {viewReserves, viewReservesByUser, createReserves, deleteReserve} = require('../controllers/ReservesController')
-const authMiddleware = require('../middleware/authMiddleware')
+const authMiddleware = require('../middleware/authMidleware')
 
-router.get('/reserves/:id', authMiddleware, viewReserves)
-router.get('/reserves', authMiddleware, viewReservesByUser)
+router.get('/reserves', authMiddleware, viewReserves)
+router.get('/reserves/user/:userId/cart/:cartId', authMiddleware, viewReservesByUser)
 
 router.post('/reserves/register', authMiddleware, createReserves)
-
-// router.patch('')
 
 router.delete('/reserves/:id/delete', authMiddleware, deleteReserve)
 
