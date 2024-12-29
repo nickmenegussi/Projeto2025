db538143e0d17acc8f9c4df08fba5981f4622236
nickmenegussi <nicolasmramos09@gmail.com>
2024-12-29 15:53:22 -0300

    Adicionando o NativeWind ao projeto e atualizando o layout da página inicial para utilizar classes do Tailwind

--- FrontEnd/app/index.jsx
+++ FrontEnd/app/index.jsx
@@ -4,19 +4,10 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center bg-white"> 
       <Text>Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'} }>Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-})
