ba0f7be396ed0cf759066f79fe0822af19db112f
nickmenegussi <nicolasmramos09@gmail.com>
2024-12-30 11:11:55 -0300

    instalando pacotes necessários para a configuração do tailwindcss no react native e fazendo algumas alterações para tudo funcionar corretamente.

--- FrontEnd/app/_layout.jsx
+++ FrontEnd/app/_layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Slot , Stack} from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
-
+import "../global.css"
 
 const RootLayout = () => {
   return (
--- FrontEnd/app/index.jsx
+++ FrontEnd/app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Link } from 'expo-router'
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white"> 
-      <Text>Aora!</Text>
+    <View className="flex-1 justify-center items-center bg-t"> 
+      <Text className="text-3xl">Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'} }>Go to Profile</Link>
     </View>
