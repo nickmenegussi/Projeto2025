e4e014e25485d0f24115e9193c5ebeca0712c642
nickmenegussi <nicolasmramos09@gmail.com>
2024-12-31 21:34:37 -0300

    Adicionando novas páginas de configuração de perfil nas configurações do aplicativo e ajustando algumas páginas que faltaram..

---
+++ FrontEnd/app/(tabs)/settings/reportAProblem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ReportAProblem = () => {
+  return (
+    <View>
+      <Text>ReportAProblem</Text>
+    </View>
+  )
+}
+
+export default ReportAProblem
