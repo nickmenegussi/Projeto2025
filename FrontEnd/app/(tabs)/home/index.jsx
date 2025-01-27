c577a533298c1f3099fcbe7f333434f9e2cf38a2
nickmenegussi <nicolasmramos09@gmail.com>
2025-01-27 14:13:03 -0300

    Add Trending and EmptyContent components to Home screen

--- FrontEnd/app/(tabs)/home/index.jsx
+++ FrontEnd/app/(tabs)/home/index.jsx
@@ -4,7 +4,8 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {Bell, CircleUserRoundIcon, Menu, MenuIcon} from 'lucide-react-native'
 import ButtonIcons from '../../../components/ButtonIcons'
-
+import Trending from '../../../components/Trending'
+import EmptyContent from '../../../components/EmptyContent'
 
 const Home = () => {
 
@@ -15,7 +16,16 @@ const Home = () => {
       locations={[0, 4]} 
       style={styles.linearGradient}
     >
-  {/* Eu defino no meu componente as cores e o tamanho dele, depois, eu coloco desistruturo o meu arrow function em uma função que irá receber um obejeto com algumas características que depois irei passar no Icon*/}
+      <SafeAreaView style={styles.safeAreaView}>
+        <FlatList
+        // substituir aqui por uma data de verdade
+        // data={[{id: 1}, {id: 2}, {id: 3}]}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <Text>{item.id}</Text>}
+        ListHeaderComponent={() => (
+            <View contentContainerStyle={styles.Container}>
+              <View style={styles.containerIcons}>
+                {/* Eu defino no meu componente as cores e o tamanho dele, depois, eu coloco desistruturo o meu arrow function em uma função que irá receber um obejeto com algumas características que depois irei passar no Icon*/}
                 <ButtonIcons color={"white"} size={30} Icon={({color, size}) => (
                   <MenuIcon color={color} size={size} />
                 )} />
@@ -28,7 +38,13 @@ const Home = () => {
                   )} />
                 </View>
               </View>
-
+              <Trending posts={[{id: 1}, {id: 2}, {id: 3}]} />
+            </View>)}
+  
+        ListEmptyComponent={() => (
+          <EmptyContent title={"Não encontrado!"} />
+        )}
+        />
       </SafeAreaView>
 
     </LinearGradient>
@@ -37,11 +53,6 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-  ScrollContainerHorizontal: {
-    flexDirection: 'row',
-    alignItems: 'start',
-    gap: 15
-  },
   linearGradient: {
     flex: 1,
   },
@@ -55,19 +66,7 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingVertical: 20,
   },
-  IconsContent: {
-    flexDirection: 'row',
-    gap: 15
-  },
-  containerIcons: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  text: {
-    color: '#FFFFFF',
-    textAlign: 'start',
-  }, ButtonContainer: {
-    width: 45,
-  },
-
+  textLink: {
+    color: '#34C759'
+  }
+})
