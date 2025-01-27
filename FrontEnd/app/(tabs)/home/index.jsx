a9571d8065f5f381c8f54ff6750dd7419448c859
nickmenegussi <nicolasmramos09@gmail.com>
2025-01-27 11:05:29 -0300

    Adicionando novos componentes CaroseulItems, EmptyContent e Trending, além de integrar o componente Trending na tela inicial e ajustar a navegação.

--- FrontEnd/app/(tabs)/home/index.jsx
+++ FrontEnd/app/(tabs)/home/index.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
-import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {Bell, CircleUserRoundIcon, Menu, MenuIcon} from 'lucide-react-native'
 import ButtonIcons from '../../../components/ButtonIcons'
+import Trending from '../../../components/Trending'
+import EmptyContent from '../../../components/EmptyContent'
 
-const forgottenPassword = () => {
+const Home = () => {
 
   return (
-    
     <LinearGradient 
       colors={['#003B73', '#60A3D9']} 
       end={{ x: 0, y: 1 }} 
@@ -16,36 +17,54 @@ const forgottenPassword = () => {
       style={styles.linearGradient}
     >
       <SafeAreaView style={styles.safeAreaView}>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-           <View>
-            <View style={styles.containerIcons}>
-              {/* Eu defino no meu componente as cores e o tamanho dele, depois, eu coloco desistruturo o meu arrow function em uma função que irá receber um obejeto com algumas características que depois irei passar no Icon*/}
-              <ButtonIcons color={"white"} size={30} Icon={({color, size}) => (
-                <MenuIcon color={color} size={size} />
-              )} />
-              <View style={styles.IconsContent}>
+        <FlatList
+        // substituir aqui por uma data de verdade
+        // data={[{id: 1}, {id: 2}, {id: 3}]}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <Text>{item.id}</Text>}
+
+        ListHeaderComponent={() => (
+            <View contentContainerStyle={styles.Container}>
+              <View style={styles.containerIcons}>
+                {/* Eu defino no meu componente as cores e o tamanho dele, depois, eu coloco desistruturo o meu arrow function em uma função que irá receber um obejeto com algumas características que depois irei passar no Icon*/}
                 <ButtonIcons color={"white"} size={30} Icon={({color, size}) => (
-                  <Bell color={color} size={size} />
+                  <MenuIcon color={color} size={size} />
                 )} />
-                <ButtonIcons color={"white"} size={30} Icon={({color, size}) => (
-                  <CircleUserRoundIcon color={color} size={size} />
-                )} />
+                <View style={styles.IconsContent}>
+                  <ButtonIcons color={"white"} size={30} Icon={({color, size}) => (
+                    <Bell color={color} size={size} />
+                  )} />
+                  <ButtonIcons color={"white"} size={30} Icon={({color, size}) => (
+                    <CircleUserRoundIcon color={color} size={size} />
+                  )} />
+                </View>
               </View>
-            </View>
-           </View>
-        </ScrollView>
+
+              <Trending posts={[{id: 1}, {id: 2}, {id: 3}]} />
+            </View>)}
+  
+        ListEmptyComponent={() => (
+          <EmptyContent title={"Não encontrado!"} />
+        )}
+
+        />
       </SafeAreaView>
 
     </LinearGradient>
   )
 }
-export default forgottenPassword
+export default Home
 
 const styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
   },
-  scrollContainer: {
+  safeAreaView: {
+    flexGrow: 1,
+    padding: 10,
+    paddingVertical: 20
+  },
+  Container: {
     flexGrow: 1,
     padding: 10,
     paddingVertical: 20,
