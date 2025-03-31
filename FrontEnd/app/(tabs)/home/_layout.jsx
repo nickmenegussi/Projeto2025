b7cf2afc60300ffd7a87bb7b6d9fdf2da5f78eac
nickmenegussi <nicolasmramos09@gmail.com>
2025-03-31 10:09:00 -0300

    adicionando novas funcionalidades para algumas  páginas

--- FrontEnd/app/(tabs)/home/_layout.jsx
+++ FrontEnd/app/(tabs)/home/_layout.jsx
@@ -7,6 +7,7 @@ const RootLayout = () => {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false}} />
       <Stack.Screen name="lectures" options={{ headerShown: false}} />
+      <Stack.Screen name="aboutLecture" options={{ headerShown: false}} />
 
     </Stack>
   )
--- FrontEnd/app/(tabs)/home/aboutLecture.jsx
+++ FrontEnd/app/(tabs)/home/aboutLecture.jsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 
 const aboutLecture = () => {
   return (
-    <View>
-      <Text>aboutLecture</Text>
-    </View>
+  
+  <ScrollView>
+    <ImageBackground imageStyle={styles.imageStyle} style={styles.background} source={require('../../../assets/images/Jesus-Cristo.png')}>
+      <View>
+      </View>
+    </ImageBackground>
+    </ScrollView>
   )
 }
 
 export default aboutLecture
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 400,
+  }, imageStyle: {
+    width: 700
+  }
+})
