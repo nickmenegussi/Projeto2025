7fdfc7a815e6377551c0201dcf9188a9edd149cb
nickmenegussi <nicolasmramos09@gmail.com>
2025-04-24 21:34:16 -0300

    feat: Implementar a tabela ReviewSociety e as funções de criação e visualização de avaliações; adicionar validação e tratamento de erros

--- BackEnd/src/controllers/ReviewSociety.js
+++ BackEnd/src/controllers/ReviewSociety.js
@@ -1,11 +1,58 @@
 const connection = require('../config/db')
 
 exports.CreateReviewSociety = (req, res) => {
+    const { descriptionReview, ratingReview, userId } = req.body
 
+    if(!descriptionReview || !ratingReview || !userId){
+        return res.status(400).json({
+            message: 'Preencha todos os campos.',
+            success: false
+        })
+    }
+
+    connection.query(`SELECT * FROM ReviewSociety WHERE descriptionReview = ? and ratingReview and userId = ?`, [descriptionReview, ratingReview, userId], (err, result) => {
+        if(err){
+            return res.status(500).json({
+                message: 'Erro ao se conectar com o servidor.',
+                success: false,
+                data: err
+            })
+        }
+
+        if(result.length > 0){
+            return res.status(400).json({
+                message: 'Você já fez uma avaliação .',
+                success: false
+            })
+        }
+
+        connection.query(`INSERT INTO ReviewSociety (descriptionReview, ratingReview, userId) VALUES (?, ?, ?)`, [descriptionReview, ratingReview, userId], (err, result) => {
+            if(err){
+                return res.status(500).json({
+                    message: 'Erro ao se conectar com o servidor.',
+                    success: false,
+                    data: err
+                })
+            }
+
+            return res.status(200).json({
+                message: 'Avaliação criada com sucesso.',
+                success: true,
+                data: result
+            })
+        })
+    })
 }
 
 exports.ViewReviewSociety = (req, res) => {
-    connection.query('SELECT * FROM ReviewSociety', (err,result) => {
+    const sortOrder = req.query.sortOrder === 'newSet' ? 'ASC' : 'DESC'
+
+    connection.query(`
+        SELECT idReviewSociety, descriptionReview, ratingReview, userId, create_at, nameUser, image_profile FROM ReviewSociety as r
+        INNER JOIN User as u on u.idUser = r.userId
+        ORDER BY create_at ${sortOrder}
+
+        `, (err,result) => {
         if(err){
             return res.status(500).json({
                 message: 'Erro ao se conectar com o servidor.',
--- BackEnd/src/routers/ReviewRouter.js
+++ BackEnd/src/routers/ReviewRouter.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {ViewReviewSociety} = require('../controllers/ReviewSociety')
+const {ViewReviewSociety, CreateReviewSociety} = require('../controllers/ReviewSociety')
 const authMiddleware = require('../middleware/authMidleware')
 const verifyPermission = require('../middleware/roleMiddleware')
 
 router.get('/reviewSociety', authMiddleware, verifyPermission(['SuperAdmin', 'Admin', 'User']), ViewReviewSociety)
+router.post('/reviewSociety/create', authMiddleware, verifyPermission(['SuperAdmin', 'Admin', 'User']), CreateReviewSociety)
 
 module.exports = router
